fd6afc1aeaf5804d08cd79a46f3b868c9905f3c1
gero-arizaga <garizaga@udesa.edu.ar>
2023-03-31 15:54:08 -0300

    db completando

--- proyecto-integrador/db/camisetas.js
+++ proyecto-integrador/db/camisetas.js
@@ -1,5 +1,67 @@
 const camisetas={
-lista:[]
+lista:[
+    
+    {
+        club : "Boca",
+        anio : "",
+        sponsor: "",
+        retro: true
+    },
+    {
+        club : "Boca",
+        anio : "",
+        sponsor: "",
+        retro: true
+    },
+    {
+        club : "Boca",
+        anio : "",
+        sponsor: "",
+        retro: false
+    },
+    {
+        club : "River",
+        anio : "",
+        sponsor: "",
+        retro: true
+    },
+    {
+        club : "River",
+        anio : "",
+        sponsor: "",
+        retro: true
+    },
+    {
+        club : "River",
+        anio : "",
+        sponsor: "",
+        retro: false
+    },
+    {
+        club : "Independiente",
+        anio : "",
+        sponsor: "",
+        retro: true
+    },
+    {
+        club : "Independiente",
+        anio : "",
+        sponsor: "",
+        retro: false
+    },
+    {
+        club : "Racing",
+        anio : "",
+        sponsor: "",
+        retro: true
+    },
+    {
+        club : "Racing",
+        anio : "",
+        sponsor: "",
+        retro: false
+    }
+]
 
 }
 
