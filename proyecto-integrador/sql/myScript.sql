1d0b91ab8f8135f6460ea9c1bf6ce9cadb97d45d
SantinoG19 <gallis@udesa.edu.ar>
2023-04-10 09:40:25 -0300

    sql

--- proyecto-integrador/sql/myScript.sql
+++ proyecto-integrador/sql/myScript.sql
@@ -1,2 +1,15 @@
 -- crear base de datos --
 -- Un error te caga el proyecto --
+use proyecto;
+create table productos(
+id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+NombreProd VARCHAR (200) NOT NULL,
+descripcion VARCHAR (200) NOT NULL,
+
+
+createdAt  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+
+
+
+);
