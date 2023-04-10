b7bdd3e5a41b45e50357356bdb3389deba91cba9
gero-arizaga <garizaga@udesa.edu.ar>
2023-04-10 15:06:07 -0300

    m

--- proyecto-integrador/db/camisetas.js
+++ proyecto-integrador/db/camisetas.js
@@ -9,72 +9,84 @@ productos:[
 
     {
         club : "Boca",
+        descripcion: "",
         anio : "1993",
         sponsor: "Parmalat",
         retro: true
     },
     {
         club : "Boca",
+        descripcion: "",
         anio : "2000",
         sponsor: "Quilmes",
         retro: true
     },
     {
         club : "Boca",
+        descripcion: "",
         anio : "2023",
         sponsor: "-",
         retro: false
     },
     {
         club : "River",
+        descripcion: "",
         anio : "1986",
         sponsor: "Fate O",
         retro: true
     },
     {
         club : "River",
+        descripcion: "",
         anio : "2003",
         sponsor: "Budweiser",
         retro: true
     },
     {
         club : "River",
+        descripcion: "",
         anio : "2023",
         sponsor: "Codere",
         retro: false
     },
     {
         club : "Independiente",
+        descripcion: "",
         anio : "2013",
         sponsor: "TCL",
         retro: true
     },
     {
         club : "Independiente",
+        descripcion: "",
         anio : "2023",
         sponsor: "-",
         retro: false
     },
     {
         club : "Racing",
+        descripcion: "",
         anio : "2015",
         sponsor: "Dueño",
         retro: true
     },
     {
         club : "Racing",
+        descripcion: "",
         anio : "2023",
         sponsor: "Aeroset",
         retro: false
     },
     {
         club : "San Lorenzo",
+        descripcion: "",
         anio : "2014",
         sponsor: "Banco Ciudad",
         retro: true
     },
     {
         club : "San Lorenzo",
+        descripcion: "",
         anio : "2023",
         sponsor: "-",
         retro: false
