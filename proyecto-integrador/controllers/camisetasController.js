51928fb1c1eb828045a408078f484dcf5523cff9
SantinoG19 <gallis@udesa.edu.ar>
2023-05-08 12:17:18 -0300

    eliminar archivo para poder unir proyecto base

--- proyecto-integrador/controllers/camisetasController.js
+++ proyecto-integrador/controllers/camisetasController.js
@@ -5,15 +5,14 @@ const data = require('../db/data')
     const camisetasController = {
     index: function(req,res){
    return res.render('product-add',{
-    usuarioLogueado: false
+   
    })
 },
 remera: function(req,res){
     return res.render('product',{
-     usuarioLogueado: false, 
-     camiseta: data.productos, 
-     comentario: data.comentarios,
-     perfil:data.usuario,
+        camiseta: data.productos, 
+        comentario: data.comentarios,
+        perfil:data.usuario,
      
 
     
@@ -22,18 +21,16 @@ remera: function(req,res){
  },
  add: function(req,res){
     return res.render('product-add',{
-        usuarioLogueado: true,
-        perfil:data.usuario,
+         perfil:data.usuario,
 
 
     })
  },
  search: function(req,res){
     return res.render('search-results',{
-        usuarioLogueado:true,
         camiseta: data.productos, 
-     comentario: data.comentarios,
-     perfil:data.usuario
+        comentario: data.comentarios,
+        perfil:data.usuario
 
     } )
 }}
