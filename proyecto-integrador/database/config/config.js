425981525f02ad364b8d8251679b6405b3b1ca9c
geroarizaga <garizaga@udesa.edu.ar>
2023-05-08 12:19:05 -0300

    borrando para unirnos

---
+++ proyecto-integrador/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "development": {
+      "username": "root",
+      "password": null, //"root" para usuarios de Mac
+      "database": "baseDatos1",//ojo acá poner bien la base de datos//
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "test": {
+      "username": "root",
+      "password": null,
+      "database": "database_test",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "production": {
+      "username": "root",
+      "password": null,
+      "database": "database_production",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    }
+  }
