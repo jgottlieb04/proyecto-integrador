dbb59ef2d15e3069dd089d1cd8c3b8df18632158
SantinoG19 <gallis@udesa.edu.ar>
2023-05-29 09:52:04 -0300

    hola

--- proyecto-integrador/database/models/Product.js
+++ proyecto-integrador/database/models/Product.js
@@ -1,7 +1,7 @@
 module.exports= function (sequelize, dataTypes){
     let alias="Product";
     //mapeo exacto de cada una de las columnas//
-    let col = {
+    let cols = {
         id:{
             autoIncrement: true,
             primaryKey: true,
