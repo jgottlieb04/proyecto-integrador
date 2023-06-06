9e37dfc25976ba0085c0fb72ce21078381bf1494
jgottlieb04 <jgottlieb@udesa.edu.ar>
2023-06-06 19:28:33 -0300

    buscador

--- proyecto-integrador/controllers/camisetasController.js
+++ proyecto-integrador/controllers/camisetasController.js
@@ -1,12 +1,12 @@
+const { Sequelize } = require('../database/models');
 const db = require('../database/models')
-const camiseta =db.Product //alias del modelo
-let op=db.sequelize.op;
-
+const Product =db.Product //alias del modelo
+let op= db.Sequelize.Op
 const camisetasController = {
 
     detalle: (req,res) => {
         let id= req.params.id;
-        camiseta.findbyPK(id)
+        Product.findbyPK(id)
          .then(function(result){
             return res.render('product',{
                 productos: result
@@ -21,6 +21,22 @@ const camisetasController = {
         })
     },
     search: function(req,res){
+        let busqueda=req.query.search;
+        Product.findall(
+            {
+                where:[{
+                    descripcion: {[op.like]:`%${busqueda}%`}}
+                ]
+            }
+        ) .then(function(result){
+            return res.render('product',{
+                productos: result
+            })        
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+        
         return res.render('search-results',{
             productos: result, 
             comentarios: result,
