3a39831b3b4322f272be90562447482d1138f029
jgottlieb04 <jgottlieb@udesa.edu.ar>
2023-06-07 10:05:06 -0300

    borro node modukes

--- proyecto-integrador/controllers/indexControllers.js
+++ proyecto-integrador/controllers/indexControllers.js
@@ -26,15 +26,4 @@ const indexController = {
 
 
 
-/*const controller = {
-    index: function(req, res){
-        res.render('index',{
-            productos:data.productos,
-            comentarios:data.comentarios,
-            usuario:data.usuario
-        })
-        
-    }
-}
-*/
 module.exports=indexController
