e2afc0b913c8446351451620d4281d45e3430fbc
jgottlieb04 <jgottlieb@udesa.edu.ar>
2023-06-08 10:51:18 -0300

    d

--- proyecto-integrador/database/models/Product.js
+++ proyecto-integrador/database/models/Product.js
@@ -29,7 +29,7 @@ module.exports= function (sequelize, dataTypes){
     };
     let config = {
         tableName: 'productos',
-        timestamps: true, //Si la tabla no tiene los campos created_at y updated_at
+        timestamps: false, //Si la tabla no tiene los campos created_at y updated_at
         underscored: true, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.
     };
     
